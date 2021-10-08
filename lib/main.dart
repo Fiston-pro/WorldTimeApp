e231b007fe550b817d84fbd1c65a0a996c07f8d1
Fiston-pro <byiringiroetienne2@gmail.com>
2021-10-08 07:56:42 +0200

    Added 3 routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:worldtimeapp/pages/Home.dart';
+import 'package:worldtimeapp/pages/Loading.dart';
+import 'package:worldtimeapp/pages/Locations.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home()
+    initialRoute: '/',
+    routes: {
+      '/': (context)=> Loading(),
+      '/Home': (context)=> Home(),
+      '/Locations': (context)=> Locations()
+  },
   ));
 }
 
