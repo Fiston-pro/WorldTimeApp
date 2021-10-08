faa7813288b72ab83350909b5669fcb12721cf3f
Fiston-pro <byiringiroetienne2@gmail.com>
2021-10-08 07:06:54 +0200

    I initialised the project initialised 3 pages: home, locations and loading

---
+++ lib/pages/Locations.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Locations extends StatefulWidget {
+  const Locations({Key? key}) : super(key: key);
+
+  @override
+  _LocationsState createState() => _LocationsState();
+}
+
+class _LocationsState extends State<Locations> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Text('Locations')
+    );
+  }
+}
